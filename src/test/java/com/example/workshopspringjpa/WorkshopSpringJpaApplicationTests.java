7fd9acaffe8a43fe59a333d0dcc4f26a4da50ad5
xandy <>
2025-01-14 14:39:58 -0400

    /feature: implement User

---
+++ src/test/java/com/example/workshopspringjpa/WorkshopSpringJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.workshopspringjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopSpringJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
