d19edd2b08354c0bd599903aeec58deb6af7df45
xandy <>
2025-01-14 16:25:06 -0400

    /feature: UserService and route to /users/{id}

--- src/main/java/com/example/workshopspringjpa/resources/UserResource.java
+++ src/main/java/com/example/workshopspringjpa/resources/UserResource.java
@@ -1,17 +1,32 @@
 package com.example.workshopspringjpa.resources;
 
+import com.example.workshopspringjpa.services.UserServices;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.example.workshopspringjpa.entities.User;
 
+import java.util.List;
+
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
-    @GetMapping("")
-    public ResponseEntity<User> findAll() {
-        User u = new User(1L, "Maria", "maria#gmail.com", "999999", "12345");
-        return ResponseEntity.ok().body(u);
+    @Autowired
+    private UserServices services;
+
+    @GetMapping()
+    public ResponseEntity<List<User>> findAll(){
+        List<User> list = services.findAll();
+        return ResponseEntity.ok().body(list);
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id){
+        User user = services.findById(id);
+        return ResponseEntity.ok().body(user);
+    }
+
 }
